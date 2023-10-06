fc6778c25a032a410e300ebea9a76cd4a01648b4
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-06 11:28:37 +0200

    Add primeng.ts

---
+++ src/app/primeng.ts
@@ -0,0 +1,13 @@
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
+import { CalendarModule } from 'primeng/calendar';
+
+export const primeng = [
+    ButtonModule,
+    ToastModule,
+    TableModule,
+    PaginatorModule,
+    CalendarModule
+]
