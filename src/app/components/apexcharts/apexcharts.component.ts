37c3c5792b060351bd604170866bceba7ec74463
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-06 11:28:22 +0200

    Add apexcharts.component.ts

---
+++ src/app/components/apexcharts/apexcharts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apexcharts',
+  templateUrl: './apexcharts.component.html',
+  styleUrls: ['./apexcharts.component.css']
+})
+export class ApexchartsComponent {
+
+}
