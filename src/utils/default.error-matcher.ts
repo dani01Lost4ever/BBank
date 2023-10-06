783e5f5455bf93fe336be3ee27df3b057cfb2934
dani01Lost4ever <daniel.busetto.db@gmail.com>
2023-10-06 11:28:44 +0200

    Add default.error-matcher.ts

---
+++ src/utils/default.error-matcher.ts
@@ -0,0 +1,14 @@
+import { FormControl, FormGroupDirective, NgForm } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+    isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      return !!(control && control.invalid && (control.dirty || control.touched));
+    }
+}
+
+export class ConfirmStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return !!(control && control.invalid && (control.dirty || control.touched)) || form?.errors?.['passwordMissmatch'];
+  }
+}
